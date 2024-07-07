6d0dcbbfd7d0bbf05900b023e0b4cfddfa835ea6
Baka_Day <bakaday@gmail.com>
2024-07-07 12:08:07 -0400

    EmailJS working
    email js is working and so is everything else so far

--- contactForm.js
+++ contactForm.js
@@ -1,16 +1,15 @@
-import emailjs from 'emailjs';
-
 export function initializeContactForm() {
     const form = document.getElementById('contact-form');
     emailjs.init({
-        PublicKey: '1oZm-RJNnJc_9a2it'
-    })
+        publicKey: "1oZm-RJNnJc_9a2it",
+    });
 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
         const name = document.getElementById('name').value.trim();
         const email = document.getElementById('email').value.trim();
         const message = document.getElementById('message').value.trim();
+        const form = document.getElementById("contact-form")
 
         if (!name || !email || !message) {
             showMessage('Please fill in all fields.', 'error');
@@ -23,12 +22,8 @@ export function initializeContactForm() {
         }
 
         // Send the email using EmailJS
-        emailjs.sendForm(process.env.REACT_APP_EMAILJS_SERVICE_ID, process.env.REACT_APP_EMAILJS_TEMPLATE_ID, {
-            name: name,
-            email: email,
-            message: message
-        }, process.env.REACT_APP_EMAILJS_USER_ID)
-            .then((response) => {
+        emailjs.sendForm('service_6aqc1ps', 'template_9y0wo8q', form)
+            .then(() => {
                 showMessage('Form submitted successfully!', 'success');
                 form.reset();
             }, (error) => {
